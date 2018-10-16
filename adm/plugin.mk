8c8441724e26829a565b1dec19c095cefe6e88b3
Fabien MARTY <fabien.marty@gmail.com>
2018-10-16 09:52:43 +0200

    feat: better plugin cleaning (again)

--- adm/plugin.mk
+++ adm/plugin.mk
@@ -32,6 +32,7 @@ all: $(PREREQ) custom $(DEPLOY)
 
 clean::
 	rm -Rf local *.plugin *.tar.gz python?_virtualenv_sources/*.tmp python?_virtualenv_sources/src python?_virtualenv_sources/freezed_requirements.* python?_virtualenv_sources/tempolayer*
+	find . -type d -name "__pycache__" -exec rm -Rf {} \; >/dev/null 2>&1 || true
 
 custom::
 	echo "override me" >/dev/null
